77e471904f274c6736912ac4fab2471ae5a8576c
DmytroOrtymd <107140865+DmytroOrtymd@users.noreply.github.com>
2026-05-07 12:47:00 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,55 @@
+// config.js
+var platforms = [
+    {
+        "name": "WhiteBIT",
+        "apy": 11.52,
+        "url": "https://whitebit.com/referral/761864d2-aeb6-4a07-9543-7a65dd0eef8d",
+        "factors": {
+            "longevity": 6,     
+            "reserves": 5,     
+            "collateral": 10,         
+            "audit": 5,       
+            "yieldGap": 3,          
+            "source": 9,          
+        }
+    },
+    {
+        "name": "Neverless",
+        "apy": 2.42,
+        "url": "https://neverless.com/", // Updated URL
+        "factors": {
+             "longevity": 2,     
+            "reserves": 1,     
+            "collateral": 10,         
+            "audit": 1,       
+            "yieldGap": 9,          
+            "source": 9, 
+        }
+    },
+{
+        "name": "Bybit",
+        "apy": 0.3,
+        "url": "https://neverless.com/", // Updated URL
+        "factors": {
+             "longevity": 7,     
+            "reserves": 9,     
+            "collateral": 10,         
+            "audit": 9,       
+            "yieldGap": 9,          
+            "source": 9, 
+        }
+    },
+    {
+        "name": "Binance",
+        "apy": 0.04,
+        "url": "https://www.binance.com/", // Updated URL
+        "factors": {
+            "longevity": 8,     
+            "reserves": 9,     
+            "collateral": 10,         
+            "audit": 6,       
+            "yieldGap": 10,          
+            "source": 9, 
+        }
+    }
+];
