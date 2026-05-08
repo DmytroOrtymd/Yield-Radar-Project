7e9ae7f2b035ebbd6552cbdd0e1470cfc84f2bca
DmytroOrtymd <107140865+DmytroOrtymd@users.noreply.github.com>
2026-05-08 07:04:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,53 +3,62 @@ var platforms = [
     {
         "name": "WhiteBIT",
         "apy": 11.52,
+        "depositType": "Flexible",
+        "geckoId": "whitebit",
         "url": "https://whitebit.com/referral/761864d2-aeb6-4a07-9543-7a65dd0eef8d",
         "factors": {
-            "longevity": 6,     
-            "reserves": 5,     
-            "collateral": 10,         
-            "audit": 5,       
-            "yieldGap": 3,          
-            "source": 9,          
+            "longevity": 6,
+            "reserves": 5,
+            "collateral": 10,
+            "audit": 5,
+            "yieldGap": 3,
+            "source": 9,
         }
     },
     {
         "name": "Neverless",
         "apy": 2.42,
-        "url": "https://neverless.com/referral?code=fortunecaptain/", // Updated URL
+        "depositType": "Flexible",
+        "geckoId": null,
+        "url": "https://neverless.com/referral?code=fortunecaptain/",
         "factors": {
-             "longevity": 2,     
-            "reserves": 1,     
-            "collateral": 10,         
-            "audit": 1,       
-            "yieldGap": 9,          
-            "source": 9, 
+            "longevity": 2,
+            "reserves": 1,
+            "collateral": 10,
+            "audit": 1,
+            "yieldGap": 9,
+            "source": 9,
         }
     },
-{
+    {
         "name": "Bybit",
         "apy": 0.3,
-        "url": "https://nil/", // Updated URL
+        "depositType": "Flexible",
+        "geckoId": "bybit_spot",
+        "url": "https://nil/",
         "factors": {
-             "longevity": 7,     
-            "reserves": 9,     
-            "collateral": 10,         
-            "audit": 9,       
-            "yieldGap": 9,          
-            "source": 9, 
+            "longevity": 7,
+            "reserves": 9,
+            "collateral": 10,
+            "audit": 9,
+            "yieldGap": 9,
+            "source": 9,
         }
     },
     {
         "name": "Binance",
         "apy": 0.04,
-        "url": "https://www.binance.com/", // Updated URL
+        "depositType": "Flexible / Fixed",
+        "geckoId": "binance",
+        "url": "https://www.binance.com/",
         "factors": {
-            "longevity": 8,     
-            "reserves": 9,     
-            "collateral": 10,         
-            "audit": 6,       
-            "yieldGap": 10,          
-            "source": 9, 
+            "longevity": 8,
+            "reserves": 9,
+            "collateral": 10,
+            "audit": 6,
+            "yieldGap": 10,
+            "source": 9,
         }
     }
 ];
+
