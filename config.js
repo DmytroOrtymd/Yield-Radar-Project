02df32ec452b011990700b8dfbf4902f45a51bab
DmytroOrtymd <107140865+DmytroOrtymd@users.noreply.github.com>
2026-05-08 07:39:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,64 +1,63 @@
 // config.js
 var platforms = [
-    {
-        "name": "WhiteBIT",
-        "apy": 11.52,
-        "depositType": "Flexible",
-        "geckoId": "whitebit",
-        "url": "https://whitebit.com/referral/761864d2-aeb6-4a07-9543-7a65dd0eef8d",
-        "factors": {
-            "longevity": 6,
-            "reserves": 5,
-            "collateral": 10,
-            "audit": 5,
-            "yieldGap": 3,
-            "source": 9,
-        }
-    },
-    {
-        "name": "Neverless",
-        "apy": 2.42,
-        "depositType": "Flexible",
-        "geckoId": null,
-        "url": "https://neverless.com/referral?code=fortunecaptain/",
-        "factors": {
-            "longevity": 2,
-            "reserves": 1,
-            "collateral": 10,
-            "audit": 1,
-            "yieldGap": 9,
-            "source": 9,
-        }
-    },
-    {
-        "name": "Bybit",
-        "apy": 0.3,
-        "depositType": "Flexible",
-        "geckoId": "bybit_spot",
-        "url": "https://nil/",
-        "factors": {
-            "longevity": 7,
-            "reserves": 9,
-            "collateral": 10,
-            "audit": 9,
-            "yieldGap": 9,
-            "source": 9,
-        }
-    },
-    {
-        "name": "Binance",
-        "apy": 0.04,
-        "depositType": "Flexible / Fixed",
-        "geckoId": "binance",
-        "url": "https://www.binance.com/",
-        "factors": {
-            "longevity": 8,
-            "reserves": 9,
-            "collateral": 10,
-            "audit": 6,
-            "yieldGap": 10,
-            "source": 9,
-        }
-    }
+{
+“name”: “WhiteBIT”,
+“apy”: 11.52,
+“depositType”: “Flexible”,
+“geckoId”: “whitebit”,
+“url”: “https://whitebit.com/referral/761864d2-aeb6-4a07-9543-7a65dd0eef8d”,
+“factors”: {
+“longevity”: 6,
+“reserves”: 5,
+“collateral”: 10,
+“audit”: 5,
+“yieldGap”: 3,
+“source”: 9
+}
+},
+{
+“name”: “Neverless”,
+“apy”: 2.42,
+“depositType”: “Flexible”,
+“geckoId”: null,
+“url”: “https://neverless.com/referral?code=fortunecaptain/”,
+“factors”: {
+“longevity”: 2,
+“reserves”: 1,
+“collateral”: 10,
+“audit”: 1,
+“yieldGap”: 9,
+“source”: 9
+}
+},
+{
+“name”: “Bybit”,
+“apy”: 0.3,
+“depositType”: “Flexible”,
+“geckoId”: “bybit_spot”,
+“url”: “https://www.bybit.com/”,
+“factors”: {
+“longevity”: 7,
+“reserves”: 9,
+“collateral”: 10,
+“audit”: 9,
+“yieldGap”: 9,
+“source”: 9
+}
+},
+{
+“name”: “Binance”,
+“apy”: 0.04,
+“depositType”: “Flexible / Fixed”,
+“geckoId”: “binance”,
+“url”: “https://www.binance.com/”,
+“factors”: {
+“longevity”: 8,
+“reserves”: 9,
+“collateral”: 10,
+“audit”: 6,
+“yieldGap”: 10,
+“source”: 9
+}
+}
 ];
-
